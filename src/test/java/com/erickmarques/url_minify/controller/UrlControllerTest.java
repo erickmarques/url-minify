4ad06fb513e37ecc5908918b4e55b795595609e3
Erick Marques <erick.andrade@mv.com.br>
2024-08-12 17:03:26 -0300

    test: Adicionando testes unitários a UrlController (UrlMinify).

--- src/test/java/com/erickmarques/url_minify/controller/UrlControllerTest.java
+++ src/test/java/com/erickmarques/url_minify/controller/UrlControllerTest.java
@@ -14,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 
 import static org.assertj.core.api.Assertions.assertThat;

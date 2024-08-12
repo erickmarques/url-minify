5e8f80174842810df88fd8012bcad98f2fc7b442
Erick Marques <erick.andrade@mv.com.br>
2024-08-12 17:37:16 -0300

    test: Adicionando testes unitários a ApiErrors (Redirect).

--- src/test/java/com/erickmarques/url_minify/controller/UrlControllerTest.java
+++ src/test/java/com/erickmarques/url_minify/controller/UrlControllerTest.java
@@ -1,5 +1,6 @@
 package com.erickmarques.url_minify.controller;
 
+import com.erickmarques.url_minify.factory.Constants;
 import com.erickmarques.url_minify.factory.MinifyUrlRequestFactory;
 import com.erickmarques.url_minify.factory.MinifyUrlResponseFactory;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,12 +14,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
+
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 
 /**
@@ -55,6 +62,41 @@ class UrlControllerTest {
             assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
             assertThat(result.getBody()).isNotNull();
             assertThat(result.getBody().url()).isEqualTo(response.url());
+        }
+    }
+
+    @Nested
+    class Redirect {
+
+        @Test
+        void shouldReturnFoundStatusAndLocationHeader() {
+            // Arrange
+            var headers = new HttpHeaders();
+            headers.setLocation(URI.create(Constants.LONG_URL));
+            when(urlService.redirect(any())).thenReturn(headers);
+
+            // Act
+            var response = urlController.redirect(Constants.ID);
+
+            // Assert
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+            assertThat(response.getHeaders().getLocation()).isEqualTo(URI.create(Constants.LONG_URL));
+        }
+
+        @Test
+        void shouldThrowNotFoundIfIdDoesNotExist() {
+            // Arrange
+            var msgException = "Não existe URL para o ID informado!";
+            when(urlService.redirect(any())).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, msgException));
+
+            // Act & Assert
+            ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+                urlController.redirect(any());
+            });
+
+            assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+            assertThat(exception.getReason()).isEqualTo(msgException);
+            verify(urlService).redirect(any());
         }
     }
 }

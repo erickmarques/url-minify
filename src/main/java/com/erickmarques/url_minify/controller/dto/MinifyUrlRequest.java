1b40220b49a7c2d81b4b30482b7f710f45752691
Erick Marques <erick.andrade@mv.com.br>
2024-08-17 22:33:39 -0300

    feat: Adicionando validação de URL válida para o MinifyUrlRequest.

--- src/main/java/com/erickmarques/url_minify/controller/dto/MinifyUrlRequest.java
+++ src/main/java/com/erickmarques/url_minify/controller/dto/MinifyUrlRequest.java
@@ -2,7 +2,8 @@ package com.erickmarques.url_minify.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import org.hibernate.validator.constraints.URL;
 
 @Builder
-public record MinifyUrlRequest(@NotBlank(message = "Favor informar o destino!")  String url) {
+public record MinifyUrlRequest(@URL(message = "Favor informar uma URL válida!") @NotBlank(message = "Favor informar o destino!")  String url) {
 }
